27eab573660af509c7cdcf92f1a9767fbb74b66c
Carlos Eduardo <71237690+carlosealves2@users.noreply.github.com>
2025-06-17 11:46:08 -0300

    feat: add `Remove` method to Cache interface and RedisClient implementation
    - Introduce `Remove` to delete keys from the cache.
    - Update Cache interface and implement the method in RedisClient.

--- services/backend/oidc-service/internal/infra/cache/cache.go
+++ services/backend/oidc-service/internal/infra/cache/cache.go
@@ -11,6 +11,7 @@ import (
 type Cache interface {
 	Get(ctx context.Context, key string) ([]byte, error)
 	Set(ctx context.Context, key string, value []byte, expirationTime time.Duration) error
+	Remove(ctx context.Context, key string) error
 }
 
 type CacheType rune
